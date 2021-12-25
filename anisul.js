6f6307306ddfc62e1472c4c71f151eacc6399d05
Lolit <lrp.0456@gmail.com>
2021-12-25 13:14:26 +0600

    first commit

---
+++ anisul.js
@@ -0,0 +1,266 @@
+ /*LIBERY FUNCTION
+        var text = "Bangladesh";
+        var len = text.length;
+        document.write(len);
+
+        var text = "Bangladesh";
+        text.length;
+        document.write(text.length);
+
+        var text = "Bangladesh";
+        var len = text.length;
+        document.write("Number of Character: " + len);
+
+        var text = "Bangladesh";
+        document.write("<br/>Number of Character : " + text.length);*/
+
+
+
+        /* USER INPUT
+        var text = prompt("Enter Your Name : ")
+        document.write("Number of character " + text.length);*/
+
+
+
+        /*var text = "Bangladesh";
+        var cha = text.charAt(8);
+        document.write(cha);
+
+        var text = "Bangladesh";
+        text = text.toUpperCase();
+        document.write(text);
+
+        var text = " BAngladesh";
+        text = text.toLowerCase();
+        document.write(text);*/
+
+
+        /*CONCAT
+        var text1 = "Bangladesh ";
+        var text2 = "is a Beautiful conuntry.";
+        var text = text1.concat(text2);
+        document.write(text);*/
+        
+
+        
+        /*carectar slice
+        var text1 = "Bangladesh";
+        var text = text1.slice(2,5);
+        document.write(text);*/
+
+
+
+        //TAKS
+        // var fastName = "Lolit ";
+        // var lastName = "MOhan";
+        // var fullName = fastName + lastName;
+        // document.write(fullName);
+
+        // var fullName = fullName.toLowerCase();
+        // document.write(fullName);
+        // var text = prompt("Enter Your Name: ");
+        // document.write("Number of Character : " + text.length);
+
+
+        // var fastName = prompt("Enter Your FastName :<br/> ");
+        // document.write(fastName );
+        // var lastName = prompt("Enter your lastName :");
+        // document.write(lastName);
+
+        // var len = fastName+lastName.length;
+        // document.write(len);
+
+        //CLASS: 09 Arithmetic and assignment operator
+        //Arithmetic operator : +, -, *, /, %(Modulas), **, --, ++(Exponent),
+        //assignment operator : =, +=, -=, *=, /=, %=, **=
+        
+        // var x = 8;
+        // x += 5; // x = x+x
+        // console.log(x);
+
+        // var y = 4;
+        // // y *= 3; //y = y*z; 4*3
+        // // document.write(y);
+
+        // y /= 9; //y = 4/9;
+        // document.write(y);
+
+
+
+        // var sum1 = prompt("Enter fast Number :");
+        // var sum2 = prompt("Enter las Number: ");
+
+        // sum1 = parseFloat(sum1, 10);
+        // sum2 = parseFloat(sum2, 10);
+
+        // var sum = sum1 + sum2;
+        // var sub = sum1 - sum2;
+
+        // document.write("Sum1 + sum2 = " + sum + "<br/> <br/>");
+        // document.write("Sum1 - sum2 = " + sub);
+
+
+        // var fastName = prompt("Enter your FastNameber : ");
+        // var lastName = prompt("Enter your LastNameber :");
+        // fastName = parseFloat(fastName, 10);
+        // lastName = parseFloat(lastName, 10);
+        // var sum = fastName + lastName;
+        // var sub = fastName - lastName;
+        // var times = fastName * lastName;
+        // var share = fastName / lastName;
+        // var modulas = fastName % lastName;
+        // document.write("Enter your sum = " + sum + "<br/>");
+        // document.write("Enter yiur sub = " + sub + "<br/>");
+        // document.write("Enter yiur times = " + times + "<br/>");
+        // document.write("Enter yiur share = " + share + "<br/>");
+        // document.write("Enter yiur modulas = " + modulas);
+
+
+        // TEMPERATURE CONVERT
+        /*var fahrenheit = parseFloat(prompt("enter Your templerature = "));
+        var celsus = (fahrenheit - 32) * (5/9);
+        document.write("Celsus = " + celsus);*/
+
+        // var celsus = parseFloat(prompt("Enter YOur celsus Temperature = "));
+        // var fahrenheit = (celsus * (9/5)) + 32;
+        // document.write("farhenite : " + fahrenheit);
+
+
+        // OPARETOR 
+        // 1. assign oparetor
+            // >, <, >=, <=, =, ==, ===, !=, !,
+        // 2. logical oparetor
+            // &&, ||, 
+
+
+           /* var x = 20;
+            var y = 70;
+            var z = 40;
+
+            if(x>y){
+                console.log("x is the maximum number" );
+            }
+            else if(y>z){
+                console.log("y is the maximum number");
+            }
+            else{
+                console.log("z is the maximum number");
+            }
+
+
+            // 10-30 find out odd number between 10 to 30
+
+
+            for (var i = 3; i <= 30; i++){
+               
+                if(i % 2 !=0){
+                    console.log(i);
+                }
+            }
+
+            var letter = prompt("Enter your letter : ");
+
+            letter = letter.toLowerCase();
+
+            if(letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "y"){
+                console.log("vowel");
+            }else
+                console.log("Consonated");*/
+                
+        
+                
+            // digit spelling
+
+            /*var digit = prompt("Enter any digit");
+
+            if(digit == 0)
+                console.log("Zoro");
+            else if(digit == 1)
+                console.log("One");
+            else if(digit == 2)
+                console.log("Two");
+            else if(digit == 3)
+                console.log("Three");
+            else if(digit == 4)
+                console.log("Four")
+            else if(digit == 5)
+                console.log("Five");
+            else if(digit == 6)
+                console.log("Six");
+            else if(digit == 7)
+                console.log("Seven");
+            else if(digit == 8)
+                console.log("Eight");
+            else if(digit == 9)
+                console.log("Nine");*/
+
+
+
+               /*var digit = prompt("Enter Your Digit");
+
+               switch (digit){
+                   case "0":
+                    console.log("Zero");
+                    break;
+                    case "1":
+                    console.log("One");
+                    break;
+                    case "2":
+                    console.log("Two");
+                    break;
+                    case "3":
+                    console.log("Three");
+                    break;
+                    case "4":
+                    console.log("Four");
+                    break;
+                    case "5":
+                    console.log("Five");
+                    break;
+                    case "6":
+                    console.log("Six");
+                    break;
+                    case "7":
+                    console.log("Seven");
+                    break;
+                    case "8":
+                    console.log("Eight");
+                    break;
+                    case "9":
+                    console.log("Nine");
+                    break;
+                    default :
+                    console.log("Not a digit");
+               } */
+
+
+               /*var letter = prompt("Enter your letter");
+               letter = letter.toLowerCase();
+               switch (letter){
+                   case "a":
+                    case "e":
+                    case "i":
+                    case "o":
+                    case "u":
+                        console.log("vowel");
+                        break;
+
+                    default :
+                        console.log("consonate");
+               }*/
+
+
+            //    var digit = prompt("Enter your Digit");
+
+            //    if(digit == 0)
+            //        console.log("Zero")
+            //     else if(digit == 1)
+            //         console.log("One");
+            //     else if (digit == 2)
+            //         console.log("Two");
+            //    else if (digit == 3)
+            //         console.log("Three");
+            //     else if (digit == 4)
+            //         console.log("Four");
+            //     else
+            //         console.log("Not a digit");4
---
+++ lolit.js
@@ -0,0 +1,12 @@
+var letter = prompt("Enter your letter");
+letter = letter.toLowerCase();
+
+if(letter = "a");
+    else if("vowel");
+if(letter == "e");
+    else if("vowel");
+if (letter == "i");
+    else if("vowel");
+
+else
+    ("Consonated");
