f1a6bc7073a2b3b43fb994ec20bb26fb50c3f966
Lolit <lrp.0456@gmail.com>
2021-12-27 08:21:59 +0600

    edit

--- anisul.js
+++ anisul.js
@@ -265,4 +265,15 @@
             //     else
             //         console.log("Not a digit");
 
+          
+
+            var i = 1;
+            var sum = 0;
+
+            while (i <= 10) {
+                sum = sum+i;
+                i = i + 1;
+            }
+            document.write(sum)
+            document.write(<h1>End</h1>)
             
--- lolit.js
+++ lolit.js
@@ -1,12 +1,31 @@
-var letter = prompt("Enter your letter");
-letter = letter.toLowerCase();
+// var letter = prompt("Enter your letter");
+// letter = letter.toLowerCase();
 
-if(letter = "a");
-    else if("vowel");
-if(letter == "e");
-    else if("vowel");
-if (letter == "i");
-    else if("vowel");
+// if(letter = "a");
+//     else if("vowel");
+// if(letter == "e");
+//     else if("vowel");
+// if (letter == "i");
+//     else if("vowel");
 
-else
-    ("Consonated");
+// else
+//     ("Consonated");
+
+// FOR LOOP ER BEBOHAR
+// for( var i=2; i<=100; i++){
+//     document.write(" "+i);
+// }
+
+// WHILE LOOP ER BEBOHAR 
+// 1+2+3+....10;
+
+var i = 1;
+var sum = 0;
+
+while (i <= 10) {
+    sum = sum+i;
+    i = i + 1;
+}
+document.write(sum);
+document.write(<h1>End</h1>);
+
